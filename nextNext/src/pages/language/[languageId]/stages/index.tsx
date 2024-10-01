7e4ff68ef80e5dca224517784ab0db1860755031
mohamed-yesser <yesser305@gmail.com>
2024-10-01 17:49:09 +0100

    v10

--- nextNext/src/pages/language/[languageId]/stages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import StageListWrapper from '../../../../components/games/StageListWrapper';
-
-const StagesPage: React.FC = () => {
-  return <StageListWrapper />;
-};
-
-export default StagesPage;
